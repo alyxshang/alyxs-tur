7a837af0fb4ff7588edc1cde949043b762c104b5
alyxshang <179976644+alyxshang@users.noreply.github.com>
2024-11-05 18:39:42 +0000

    Deploying to gh-pages from @ alyxshang/alyxs-tur@81193fdffad92850edfcbf1cf69cbf00820559e9 🚀

--- scripts/install-repo.sh
+++ scripts/install-repo.sh
@@ -1,9 +1,13 @@
 #!/data/data/com.termux/files/usr/bin/env bash
 
 install(){
-    mkdir $PREFIX/etc/apt/sources.list.d
+    if [ -d "$PREFIX/etc/apt/sources.list.d" ]; then
+        echo "".
+    else
+        mkdir $PREFIX/etc/apt/sources.list.d
+    fi
     touch $PREFIX/etc/apt/sources.list.d/alyxs-tur.list
-    echo "deb [trusted=yes] https://alyxshang.boo/alyxs-tur/tur" >> $PREFIX/etc/apt/sources.list.d/alyxs-tur.list
+        echo "deb [trusted=yes] https://alyxshang.boo/alyxs-tur/tur termux extras" >> $PREFIX/etc/apt/sources.list.d/alyxs-tur.list
 }
 
 install
