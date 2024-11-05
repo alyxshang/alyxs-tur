3e52ddc08657465298bf97f9e4afcb4537ecc9de
alyxshang <179976644+alyxshang@users.noreply.github.com>
2024-11-05 19:05:47 +0100

    Init.

---
+++ build.sh
@@ -0,0 +1,40 @@
+setup_env(){
+    mkdir build
+    mkdir build/packages
+    mkdir build/repo
+    cp -rf src/* build/packages
+    cd build/packages
+    wget https://raw.githubusercontent.com/termux/termux-create-package/refs/heads/master/src/termux-create-package
+    chmod a+x termux-create-package
+    mv termux-create-package termux-create-package.py
+}
+build_pkgs(){
+    for d in * ; do
+        if [ -d $d ]; then
+            echo "Building package $d"
+            cp -f termux-create-package.py $d
+            cd $d
+            python3 termux-create-package.py manifest.json
+            mv *.deb ../../repo
+            cd ..
+        fi
+    done
+    cd ..
+}
+
+build_repo(){
+    wget https://raw.githubusercontent.com/termux/termux-apt-repo/refs/heads/master/termux-apt-repo
+    mv termux-apt-repo termux-apt-repo.py
+    python3 termux-apt-repo.py repo dist
+    mv dist ..
+    cd ..
+}
+
+clean_up(){
+    rm -rf build
+}
+
+setup_env
+build_pkgs
+build_repo
+clean_up
